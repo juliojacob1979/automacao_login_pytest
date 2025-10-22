95d08e59c1373152ae1cef243cff806543c6ded7
Julio Jacob <julioccfj@gmail.com>
2025-10-22 18:40:54 -0300

    ajuste test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -14,12 +14,18 @@ def driver_setup():
     # Configurações do Chrome
     chrome_options = Options()
     
-    # Maximiza a janela
-    chrome_options.add_argument("--start-maximized")
-    
-    # Desativa logs de DevTools / GCM
-    chrome_options.add_argument("--log-level=3")           # reduz logs do Chrome
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])  # remove INFO/ERROR
+    # Se estiver rodando no GitHub Actions
+    if os.environ.get("CI") == "true":
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--disable-gpu")
+    else:
+        # Local: Chrome normal
+        chrome_options.add_argument("--start-maximized")
+        chrome_options.add_argument("--log-level=3")
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
     
     # Inicializa o driver
     driver = webdriver.Chrome(
